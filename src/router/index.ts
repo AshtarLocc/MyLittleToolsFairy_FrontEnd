6abd1b1bba47ef4ed6d20469e1966817b7d3fb62
AshtarLoc <ashtarloc@gmail.com>
2024-08-28 17:08:45 +0800

    Ash: 樹狀table 與 遞迴創建多層級菜單

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,40 +1,40 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router'
 let list = [
-  {
-    name: "login",
-    path: "/login",
-    component: () => import("../views/index/LoginPage.vue"),
-  },
-  {
-    name: "admin",
-    path: "/",
-    component: () => import("../views/index/RootPage.vue"),
-    children: [
-      {
-        name: "一級菜單",
-        path: "/first",
-        component: () => import("../views/first/first.vue"),
-      },
-      {
-        name: "二級菜單1",
-        path: "/first2-1",
-        component: () => import("../views/first/first2-1/first2-1.vue"),
-      },
-      {
-        name: "二級菜單2",
-        path: "/first2-2",
-        component: () => import("../views/first/first2-2/first2-2.vue"),
-      },
-      {
-        name: "一級菜單2",
-        path: "/second",
-        component: () => import("../views/second/second.vue"),
-      },
-    ],
-  },
-];
+	{
+		name: 'login',
+		path: '/login',
+		component: () => import('../views/index/LoginPage.vue')
+	},
+	{
+		name: 'admin',
+		path: '/',
+		component: () => import('../views/index/RootPage.vue'),
+		children: [
+			{
+				name: '一級菜單',
+				path: '/first',
+				component: () => import('../views/first/first.vue')
+			},
+			{
+				name: '二級菜單1',
+				path: '/first2-1',
+				component: () => import('../views/first/first2-1/first2-1.vue')
+			},
+			{
+				name: '二級菜單2',
+				path: '/first2-2',
+				component: () => import('../views/first/first2-2/first2-2.vue')
+			},
+			{
+				name: '一級菜單2',
+				path: '/second',
+				component: () => import('../views/second/second.vue')
+			}
+		]
+	}
+]
 const router = createRouter({
-  history: createWebHistory(),
-  routes: list,
-});
-export default router;
+	history: createWebHistory(),
+	routes: list
+})
+export default router
--- src/views/index/RootPage.vue
+++ src/views/index/RootPage.vue
@@ -1,40 +1,55 @@
 <template>
-  <div class="common-layout">
-    <el-container>
-      <el-aside width="200px">
-        <el-menu
-          active-text-color="#ffd04b"
-          background-color="#545c64"
-          class="el-menu-vertical-demo"
-          default-active="2"
-          text-color="#fff"
-          @open="handleOpen"
-          @close="handleClose"
-          router
-        >
-          <ThreeMenu></ThreeMenu>
-        </el-menu>
-      </el-aside>
-      <el-container>
-        <el-header>Header</el-header>
-        <el-main><router-view></router-view></el-main>
-      </el-container>
-    </el-container>
-  </div>
+	<div class="common-layout">
+		<el-container>
+			<el-aside width="200px">
+				<el-menu
+					active-text-color="#ffd04b"
+					background-color="#545c64"
+					class="el-menu-vertical-demo"
+					default-active="2"
+					text-color="#fff"
+					@open="handleOpen"
+					@close="handleClose"
+					router
+				>
+					<ThreeMenu :list="res"></ThreeMenu>
+				</el-menu>
+			</el-aside>
+			<el-container>
+				<el-main><router-view></router-view></el-main>
+			</el-container>
+		</el-container>
+	</div>
 </template>
 
 <script lang="ts" setup>
 import {
-  Document,
-  Menu as IconMenu,
-  Location,
-  Setting,
-} from "@element-plus/icons-vue";
-import ThreeMenu from "../../components/TreeMenu.vue";
+	Document,
+	Menu as IconMenu,
+	Location,
+	Setting
+} from '@element-plus/icons-vue'
+import ThreeMenu from '../../components/TreeMenu.vue'
+import { ref } from 'vue'
+const res = ref([
+	{
+		index: '/',
+		name: '一級菜單1',
+		children: [
+			{ index: '/first2-1', name: '二級菜單2-1', children: null },
+			{ index: '/first2-2', name: '二級菜單2-2', children: null }
+		]
+	},
+	{
+		index: '/second',
+		name: '一級菜單2',
+		children: null
+	}
+])
 const handleOpen = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath);
-};
+	console.log(key, keyPath)
+}
 const handleClose = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath);
-};
+	console.log(key, keyPath)
+}
 </script>
